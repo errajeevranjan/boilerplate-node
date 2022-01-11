8633d413b89012b555b24d186176cd3727352277
errajeevranjan <rjeevrnjn17@gmail.com>
2022-01-11 18:03:29 +0530

    Update DeleteUserProfile.js

--- src/controllers/userProfile/DeleteUserProfile.js
+++ src/controllers/userProfile/DeleteUserProfile.js
@@ -1,13 +1,15 @@
 import print_error from "../../helpers/print_error.js";
 import UserModel from "../../models/UserModel.js";
+import DecodeAccessToken from "../../helpers/tokens/DecodeAccessToken.js";
 
 const DeleteUserProfile = async (request, response, next) => {
 	try {
-		const id = request.params.id; // ? id of the user whose profile is being deleted
-
-		const result = await UserModel.findByIdAndDelete(id);
+		const authHeaders = request.headers.authorization;
+		// ? extracting id of the user whose profile is being deleted
+		const id = await DecodeAccessToken(authHeaders);
+		await UserModel.findByIdAndDelete(id);
 		// now next step is to delete the refresh token from the db before sending the response
-		response.send(result);
+		response.sendStatus(204);
 	} catch (error) {
 		print_error("19 :: Error Occurred in DeleteUserProfile", error);
 		next(error);
