31e198dfaa73dc3e1eaf32da2805e5792b894e36
errajeevranjan <rjeevrnjn17@gmail.com>
2022-01-11 17:49:52 +0530

    Update ProfileRoutes.js

--- src/routes/ProfileRoutes.js
+++ src/routes/ProfileRoutes.js
@@ -1,6 +1,4 @@
 import express from "express";
-import UserSignIn from "../controllers/userAuth/UserSignIn.js";
-import UserSignOut from "../controllers/userAuth/UserSignOut.js";
 import DeleteUserProfile from "../controllers/userProfile/DeleteUserProfile.js";
 import GetUserProfile from "../controllers/userProfile/GetUserProfile.js";
 import UpdateUserProfile from "../controllers/userProfile/UpdateUserProfile.js";
@@ -8,10 +6,10 @@ import VerifyAccessToken from "../helpers/tokens/VerifyAccessToken.js";
 
 const router = express.Router();
 
-router.get("/:id", VerifyAccessToken, GetUserProfile);
+router.get("/", VerifyAccessToken, GetUserProfile);
 
-router.patch("/:id", VerifyAccessToken, UpdateUserProfile);
+router.patch("/", VerifyAccessToken, UpdateUserProfile);
 
-router.delete("/:id", VerifyAccessToken, DeleteUserProfile);
+router.delete("/", VerifyAccessToken, DeleteUserProfile);
 
 export default router;
