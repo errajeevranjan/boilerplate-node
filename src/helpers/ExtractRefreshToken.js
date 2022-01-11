83433dd3950d6b92111942571e9c5b1417e68c34
errajeevranjan <rjeevrnjn17@gmail.com>
2022-01-11 18:50:24 +0530

    Update ExtractRefreshToken.js

--- src/helpers/ExtractRefreshToken.js
+++ src/helpers/ExtractRefreshToken.js
@@ -1,6 +1,5 @@
 //  function that takes cookie and extracts refresh token from it using substring method
-const ExtractRefreshToken = (cookie) => {
-	return cookie.substring(cookie.indexOf("=") + 1, cookie.lastIndexOf(""));
-};
+const ExtractRefreshToken = (cookie) =>
+	cookie.substring(cookie.indexOf("=") + 1, cookie.lastIndexOf(""));
 
 export default ExtractRefreshToken;
